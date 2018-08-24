486e1c639fce876e8c74ce5fa29d3003ae064337
maclops <maclops@msn.com>
2018-08-24 14:12:46 -0300

    Corrigido bug do h2, criado repository e service para Categoria

---
+++ src/main/java/com/wellington/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.wellington.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wellington.cursomc.domain.Categoria;
+import com.wellington.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
