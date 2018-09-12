7cb91094db56b2b7628c900cc74e9c21336514e8
maclops <maclops@msn.com>
2018-09-12 13:34:43 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/wellington/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.wellington.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wellington.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
