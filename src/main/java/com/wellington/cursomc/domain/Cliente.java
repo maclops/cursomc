78acc359040ff642aa6bdb543c5916fda9e686d1
maclops <lol@grr.la>
2018-10-01 14:42:09 -0300

    PUT, DELETE e GET para Clientes

--- src/main/java/com/wellington/cursomc/domain/Cliente.java
+++ src/main/java/com/wellington/cursomc/domain/Cliente.java
@@ -49,7 +49,7 @@ public class Cliente implements Serializable {
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCod(); //macete para pegar o id do enum
+		this.tipo = (tipo==null) ? null : tipo.getCod(); //macete para 'forçar' aceitar tipo nulo
 	}
 
 	public Integer getId() {
