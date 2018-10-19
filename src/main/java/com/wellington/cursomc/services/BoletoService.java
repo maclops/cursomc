73a65bdac822ff2640f757452444fc86b35a2b00
maclops <lol@grr.la>
2018-10-19 15:44:39 -0300

    Inserindo Pedido

---
+++ src/main/java/com/wellington/cursomc/services/BoletoService.java
@@ -0,0 +1,21 @@
+package com.wellington.cursomc.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.wellington.cursomc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+	
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instanteDoPedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(cal.getTime());
+		// numa chamada real, substituir implementação por uma chamada do webservice
+	}
+
+}
